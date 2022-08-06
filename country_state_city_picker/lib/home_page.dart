599a0e19def80c19b967e8cd6218af6e8b75f188
komal <kk6612465@gmail.com>
2022-08-06 05:32:57 +0530

    Country state City Picker

---
+++ country_state_city_picker/lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:csc_picker/csc_picker.dart';
+
+class MyHomeApp extends StatefulWidget {
+  const MyHomeApp({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeAppState createState() => _MyHomeAppState();
+}
+
+class _MyHomeAppState extends State<MyHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Country State City Picker"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: CSCPicker(
+          layout: Layout.vertical,
+          //flagState: CountryFlag.DISABLE,
+          onCountryChanged: (country) {},
+          onStateChanged: (state) {},
+          onCityChanged: (city) {},
+          /* countryDropdownLabel: "*Country",
+          stateDropdownLabel: "*State",
+          cityDropdownLabel: "*City",*/
+          //dropdownDialogRadius: 30,
+          //searchBarRadius: 30,
+        ),
+      ),
+    );
+  }
+}
--- medication_app/android/app/src/main/kotlin/com/example/exerciseapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.exerciseapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
